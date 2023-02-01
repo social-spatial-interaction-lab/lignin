f6adbb2ed1beece208adf33174d26d0af6def779
MrMallIronmaker <725mrm@gmail.com>
2023-01-31 20:54:41 -0800

    find enabled

---
+++ ligninapp/static/ligninapp/main.js
@@ -0,0 +1,49 @@
+
+
+const outputDivTable = $("#output-div-table");
+
+function addPaper() {
+    let paperId = $(this).attr("data-lignin-paperId");
+    console.log(paperId);
+}
+
+function stringOrFALN(keyname, entry) {
+    if (keyname === "authors") {
+        return entry["authors"].map(x => x.name).join(", ");
+    } else {
+        return entry[keyname];
+    }
+}
+function arrayToTable(array, options) {
+    const allKeys = Object.keys(array.reduce(function(acc, curr) {Object.keys(curr).forEach(x => acc[x] = true); return acc;}, {}));
+    console.log(allKeys);
+    var table = $("<table>");
+    const headerRow = $("<tr>").append(allKeys.map(keyname => $("<th>").text(keyname)).concat([$("<th>").text("add?")]))// .append($("<td>").append($("<button>")))
+    table.append(headerRow);
+    table.append(array.map(entry => $("<tr>").append(
+        allKeys.map(keyname => $("<td>").text(stringOrFALN(keyname, entry))).concat([
+            $("<td>").append($("<button>").text("add").attr("data-lignin-paperId", entry["paperId"]).click(addPaper))
+        ])
+    )));
+    return table;
+}
+
+function displayData(data) {
+    console.log(data);
+    var table = arrayToTable(data.data, {thead: true});
+
+    outputDivTable.empty();
+    outputDivTable.append(table);
+}
+
+$("#find").submit(function() {
+    const queryVal = $("#find-query").val();
+
+    $.getJSON(
+        "https://api.semanticscholar.org/graph/v1/paper/search?query=" + encodeURI(queryVal) + "&fields=title,year,authors",
+        {},
+        displayData
+        );
+
+    return false;
+});
--- ligninapp/views.py
+++ ligninapp/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Paper
 
 def index(request):
     return HttpResponse("Hello, world. You're at the index.")
 
 
 def question(request, question_id):
-    return HttpResponse("Question id" + str(question_id))
+    print(question_id)
+    return render(request, template_name="ligninapp/question.html", context={
+        "question_id": question_id
+    })
