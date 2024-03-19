6db62d206bc72241b21918dbd5d80111e0373ca3
Mark Roman Miller <725mrm@gmail.com>
2024-03-19 08:41:56 -0500

    registration is OK (email unverified)

--- lignin/urls.py
+++ lignin/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Other URL patterns ...
-    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     # More URL patterns ...
     path('', include('ligninapp.urls')),
