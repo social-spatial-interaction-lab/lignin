dcb1da055a2ae21aa25293a00fe5ef196f7e3023
Mark Roman Miller <725mrm@gmail.com>
2024-03-19 23:41:13 -0500

    updated static root

--- lignin/settings.py
+++ lignin/settings.py
@@ -13,12 +13,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 import environ
+import os
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Quick-start development settings - unsuitable for production
