20f8baab35443ce763e19855aa7615f8dd851830
Mark Roman Miller <725mrm@gmail.com>
2024-03-20 14:19:19 -0500

    added heroku db link

--- lignin/settings.py
+++ lignin/settings.py
@@ -89,12 +89,16 @@ WSGI_APPLICATION = 'lignin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
 
 
 # Password validation
