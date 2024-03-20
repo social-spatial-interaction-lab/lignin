ceb386cd2f99e1520903692ffd62feb2abdcec56
Mark Roman Miller <725mrm@gmail.com>
2024-03-20 14:14:54 -0500

    allowed hosts

--- lignin/settings.py
+++ lignin/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lignin-9b46895f6fe0.herokuapp.com"]
 
 
 # Application definition
